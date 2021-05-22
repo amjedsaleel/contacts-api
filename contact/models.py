eb1fe014341c615901f7ca10aba21d60b560bedd
Amjed Saleel K <amjedsaleel@gmail.com>
2021-05-22 22:06:06 +0530

    Media root and media url set

--- contact/models.py
+++ contact/models.py
@@ -9,9 +9,7 @@ class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
-    picture = models.ImageField(blank=True)
+    picture = models.ImageField(upload_to='pictures', blank=True)
     country_code = models.CharField(max_length=6)
     phone_number = models.CharField(max_length=15)
     is_favorite = models.BooleanField(default=False)
-
-
