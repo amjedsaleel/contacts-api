55a3f8110b972970c550df11328137a90db523ff
Amjed Saleel K <amjedsaleel@gmail.com>
2021-05-22 21:00:56 +0530

    contact model created and it registered

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
+# Django
 from django.contrib import admin
 
+# local Django
+from . models import Contact
+
 # Register your models here.
+
+admin.site.register(Contact)
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
+# django
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    picture = models.ImageField(blank=True)
+    country_code = models.CharField(max_length=6)
+    phone_number = models.CharField(max_length=15)
+    is_favorite = models.BooleanField(default=False)
+
+
