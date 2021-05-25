90f65b096e1069b0201f26153d2a554d7222e691
Amjed Saleel K <amjedsaleel@gmail.com>
2021-05-25 12:54:56 +0530

    Token authentication implemented

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,12 @@
 from django.urls import path
 
 # Rest framework
-from rest_framework.routers import DefaultRouter
+from rest_framework import authtoken
 
 # local Django
 from . import views
 
 urlpatterns = [
-    path('register/', views.RegistrationView.as_view())
+    path('register/', views.RegistrationView.as_view()),
+    path('api-token-auth/', views.CustomAuthToken.as_view())
 ]
--- contactsAPI/settings.py
+++ contactsAPI/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 
     # Installed via pip
     'rest_framework',
+
+    # Rest framework stuff
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +133,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
