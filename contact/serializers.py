f6d59eab2590d5580b06832c3b5981a615bcd923
Amjed Saleel K <amjedsaleel@gmail.com>
2021-05-25 20:49:56 +0530

    Updated contact view

--- contact/serializers.py
+++ contact/serializers.py
@@ -1,14 +1,16 @@
 # Rest framework
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 # local Django
 from . models import Contact
 
 
-class ContactSerializer(ModelSerializer):
+class ContactSerializer(serializers.ModelSerializer):
     """
     Serialize Contacts
     """
+    user = serializers.CharField(max_length=255, read_only=True)
+
     class Meta:
         model = Contact
         fields = ['id', 'user', 'first_name', 'last_name', 'picture', 'country_code', 'phone_number', 'is_favorite']
--- contact/views.py
+++ contact/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 # Rest framework
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # local Django
 from . models import Contact
@@ -13,8 +14,14 @@ from . serializers import ContactSerializer
 
 
 class Contacts(ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = ContactSerializer
     queryset = Contact.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = self.queryset.filter(user=user)
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
