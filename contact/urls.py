f04213b97dc4f819bec1d403e64fd823b6eee354
Amjed Saleel K <amjedsaleel@gmail.com>
2021-05-26 09:49:55 +0530

    Contact update, retrive

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Contacts.as_view(), name='contact')
+    path('', views.Contacts.as_view(), name='contact'),
+    path('<int:pk>/', views.ContactDetail.as_view(), name='contact-detail')
 ]
--- contact/views.py
+++ contact/views.py
@@ -2,9 +2,10 @@
 from django.shortcuts import render
 
 # Rest framework
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from  rest_framework import status
 
 # local Django
 from . models import Contact
@@ -19,9 +20,20 @@ class Contacts(ListCreateAPIView):
     queryset = Contact.objects.all()
 
     def get_queryset(self):
-        user = self.request.user
-        queryset = self.queryset.filter(user=user)
-        return queryset
+        return self.queryset.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class ContactDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ContactSerializer
+
+    def get_queryset(self):
+        return Contact.objects.filter(user=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'Status': 'Contact is deleted'}, status=status.HTTP_204_NO_CONTENT)
